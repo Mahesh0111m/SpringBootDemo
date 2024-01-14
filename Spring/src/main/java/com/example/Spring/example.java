5fa53a74978fba84c3934c5874ff2295451105b0
Mahesh0111m <maheshm0111mhm@gmail.com>
2024-01-14 19:58:44 +0530

    Autowired example

--- Spring/src/main/java/com/example/Spring/example.java
+++ Spring/src/main/java/com/example/Spring/example.java
@@ -1,12 +1,23 @@
 package com.example.Spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class example
 {
+    @Autowired
+    laptop lap;
     public void code()
+
     {
-        System.out.println("From Example");
+        //System.out.println("From Example");
+        //example to show if one class dependent on another
+        //and how in this case we can create objects for dependent classes
+        //that is by using Autowired annotation it will create objects
+        //in main file we have access to AppicationContext but outside
+        //we can use Autowired annotation to create objects
+
+        lap.compile();
     }
 }
---
+++ Spring/src/main/java/com/example/Spring/laptop.java
@@ -0,0 +1,13 @@
+package com.example.Spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class laptop
+{
+    public void compile()
+    {
+
+        System.out.println("compiling");
+    }
+}

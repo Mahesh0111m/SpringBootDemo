e82f6820033871efcaa2b66771395d48e75cd4de
Mahesh0111m <maheshm0111mhm@gmail.com>
2024-01-14 19:18:37 +0530

    demo

---
+++ Spring/src/main/java/com/example/Spring/SpringBootDemo.java
@@ -0,0 +1,15 @@
+package com.example.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemo {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringBootDemo.class, args);
+		System.out.println("Hello World");
+	}
+
+}
